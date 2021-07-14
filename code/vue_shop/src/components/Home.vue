63988b1e92bb32a54f38f7c763a6f13a0ab6756b
zhoumumu <953474446@qq.com>
2021-07-14 11:11:06 +0800

    继续完善代码功能

---
+++ code/vue_shop/src/components/Home.vue
@@ -0,0 +1,124 @@
+<template>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          :unique-opened="true"
+          :collapse= "isCollapse"
+          :collapse-transition="false"
+          :router="true"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconList[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index=" '/' + subItem.path + ''" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-menu"></i>
+              <!-- 文本 -->
+              <span>{{subItem.authName}}</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menulist: [],
+      iconList:  {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false
+    }
+  },
+  created() {
+    this.getMenuList();
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res} = await this.$http.get('menus');
+      if(res.meta.status !== 200) return this.$message.console.error(res.meta.msg);
+      this.menulist = res.data;
+      console.log(res);
+    },
+    // 点击按钮，切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = ! this.isCollapse;
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
---
+++ code/vue_shop/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  // 路由重定向
+  { path: '/', redirect: '/login'},
+  { path: '/login',component: Login },
+  { 
+    path: '/home',
+    component: Home, 
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome', component: Welcome,
+        path: '/users', component: Users
+      }
+  ]}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数 表示放行
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next();
+  // 获取token
+  else{
+    const tokenstr = window.sessionStorage.getItem('token');
+    if(!tokenstr) return next('/login')
+    else {
+      next();
+    }
+  }
+})
+
+export default router
