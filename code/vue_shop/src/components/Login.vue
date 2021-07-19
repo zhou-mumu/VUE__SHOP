94652a2304fdd0a74bf90cda31125e0644585f6c
zhoumumu <953474446@qq.com>
2021-07-19 23:58:53 +0800

    完成分类参数的开发

--- code/vue_shop/src/components/Login.vue
+++ code/vue_shop/src/components/Login.vue
@@ -65,8 +65,7 @@ export default {
                if(res.meta.status !== 200) return this.$message.error('登录失败');
                this.$message.success('登录成功');
                // 1.将登录成功后的token 保存到客户端的 sessionStoeage 中
-                // 1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
-                // 1.2 token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+                // 1.1 /.Me中
                console.log(res);
                window.sessionStorage.setItem('token',res.data.token);
                this.$router.push('/home');
